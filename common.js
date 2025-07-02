7b9d839447c11bc7b793fac3aff7dc5dd9e5106f
Soshi <robinsonkubo0345@gmail.com>
2025-07-02 22:33:31 +0900

    アニメーションを軽くした

--- common.js
+++ common.js
@@ -1,7 +1,7 @@
 // Webサイト表示時のアニメーション（ScrollRevealの設定）
 const revealCommon = {
-    duration: 2000, // アニメーションの時間（ミリ秒）
-    distance: '50px',  // 上方向に50px移動しながら表示
+    duration: 1500, // アニメーションの時間（ミリ秒）
+    distance: '30px',  // 上方向に50px移動しながら表示
     opacity: 0,        // 初期の透明度
     easing: 'ease-in-out', // 自然な加速・減速の動き
     reset: false,     // スクロールで何度もアニメーションさせたい場合はtrue
@@ -9,22 +9,14 @@ const revealCommon = {
 // 適用対象と個別設定の配列
 const revealTargets = [
     { selector: '.fade-in-up', options: { origin: 'left', delay: 0 } }, // origin 左から右へ
-    { selector: '.fade-in-up2', options: { origin: 'right', delay: 600 } }, // delay 開始までの遅延（ミリ秒）
-    { selector: '.fade-profile', options: { duration: 1500, origin: 'bottom', delay: 200 } },
+    { selector: '.fade-in-up2', options: { origin: 'right', delay: 500 } }, // delay 開始までの遅延（ミリ秒）
+    { selector: '.fade-profile', options: { origin: 'bottom', delay: 0 } },
     // { selector: '.main-grid', options: { duration: 1500, origin: 'bottom', delay: 0 } }
 ];
 // ループで一括適用
 revealTargets.forEach(({ selector, options }) => {
     ScrollReveal().reveal(selector, { ...revealCommon, ...options });
 });
-
-// モバイルパフォーマンスに重要　ページの読み込みが完了してから内部の処理を実行する
-window.addEventListener('load', () => {
-  revealTargets.forEach(({ selector,options }) => {
-    ScrollReveal().reveal(selector, { ...revealCommon, ...options })
-  });
-});
-
 
 // 問い合わせフォーム-----------------------------------------------------------
 function openForm() {
